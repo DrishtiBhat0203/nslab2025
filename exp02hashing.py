aabd65b11647084e28f5e8f3a54c0bc84bea447a
DrishtiBhat0203 <147900988+DrishtiBhat0203@users.noreply.github.com>
2025-10-27 23:48:56 +0530

    exp02hashing.py

--- exp02hashing.py
+++ exp02hashing.py
@@ -9,7 +9,7 @@ altered_text = input("Enter the tampered text: ").strip()
 hash_original = generate_sha256(original_text)
 hash_altered = generate_sha256(altered_text)
 
-# Display the results
+
 print(f"\nOriginal Text: {original_text}")
 print(f"Tampered Text: {altered_text}")
 print(f"Original Hash: {hash_original}")
